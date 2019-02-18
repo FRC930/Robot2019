a61fec346f2d90bcf686993701f9513abc8c6ce2
MasterMemeMinister <soupcateatternom@gmail.com>
2019-02-18 17:47:40 -0600

    Added LEDHandler.java

---
+++ RobotCode2019/src/main/java/frc/robot/LEDHandler.java
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+// --------- Imports --------- \\
+
+package frc.robot;
+
+public class LEDHandler {
+
+    public static enum LEDStates {
+        
+    }
+
+    static {
+
+    }
+
+    public static void init() {
+
+    }
+
+    public static void run() {
+
+    }  
+}  
