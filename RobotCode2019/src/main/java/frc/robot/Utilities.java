a392101363f1fe780d1cf72d96df1b7b7d81331e
Sam Murphy <samanthamurphy@wi.rr.com>
2019-02-18 18:49:26 -0600

    Formatted utilities

--- RobotCode2019/src/main/java/frc/robot/Utilities.java
+++ RobotCode2019/src/main/java/frc/robot/Utilities.java
@@ -18,11 +18,13 @@ import edu.wpi.first.cameraserver.CameraServer;
  *     PDP can be used by all subsystems.
  */
 public class Utilities {
+
     //Sets up a compressor
     public static Compressor compress = new Compressor(Constants.UTILITIES_COMPRESSOR_ID);
     public static final PowerDistributionPanel Power = new PowerDistributionPanel(Constants.ENDGAME_POWER_DISTRIBUTION_PANEL);
-    
+
     static {
+
         //turns on the compressor
         compress.setClosedLoopControl(true);
 
@@ -37,17 +39,22 @@ public class Utilities {
     }
     
     public static void startCapture() {
+
         // creates a thread which runs concurrently with the program
         new Thread(() -> {
+
           // Instantiate the USB cameras and begin capturing their video streams
-          UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(0);
-          UsbCamera camera2 = CameraServer.getInstance().startAutomaticCapture(1);
+          UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(Constants.UTIL_CAMERA_0_ID);
+          UsbCamera camera2 = CameraServer.getInstance().startAutomaticCapture(Constants.UTIL_CAMERA_1_ID);
     
           // set the cameras' reolutions and FPS
           camera.setResolution(Constants.CAMERA1_WIDTH, Constants.CAMERA1_HEIGHT);
           camera.setFPS(Constants.CAMERA1_FPS);
           camera2.setResolution(Constants.CAMERA2_WIDTH, Constants.CAMERA2_HEIGHT);
           camera2.setFPS(Constants.CAMERA2_FPS);
+
         }).start();
-      }
+
+    }
+
 }
