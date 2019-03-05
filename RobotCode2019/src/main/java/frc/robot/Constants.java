9f49e165aec1eb15fc63eb243127e6251a3908f6
Nathankitzman <nkitzman11@gmail.com>
2019-03-04 20:05:20 -0600

    Fixed constants

--- RobotCode2019/src/main/java/frc/robot/Constants.java
+++ RobotCode2019/src/main/java/frc/robot/Constants.java
@@ -23,6 +23,7 @@ public class Constants {
 				public static int ELEVATOR_VELOCITY = 650;
 				public static int ELEVATOR_ACCELERATION = 1200;
 				public static int ELEVATOR_REVERSE_MULTIPLIER = 1;
+				talons on elevator should be commented and victors uncommented
 		PRACTICE NUMBER 1:
 			ELEVATOR:
 				public static double ELEVATOR_KP = 6;
@@ -31,6 +32,7 @@ public class Constants {
 				public static int ELEVATOR_VELOCITY = 600;
 				public static int ELEVATOR_ACCELERATION = 500;
 				public static int ELEVATOR_REVERSE_MULTIPLIER = -1;
+				victors on elevator should be commented and talons uncommented
 	*/
 
 
@@ -47,7 +49,7 @@ public class Constants {
 	// DRIVER Controller Values-----------------------------[
 		public static final int DRIVER_CONTROLLER_ID = 0;
 
-		public static final double TRIGGER_PRESSED_VALUE_THRESHOLD = 0.4;
+		
 
 		public static final int DRIVER_BUTTON_A = 1;
 		public static final int DRIVER_BUTTON_B = 2;
@@ -64,6 +66,7 @@ public class Constants {
 		public static final int DRIVER_AXIS_LEFT_Y = 1;
 		public static final int DRIVER_AXIS_RIGHT_X = 4;
 		public static final int DRIVER_AXIS_RIGHT_Y = 5;
+		public static final double TRIGGER_PRESSED_VALUE_THRESHOLD = 0.4;
 		public static final int DRIVER_AXIS_LT = 2;
 		public static final int DRIVER_AXIS_RT = 3;	
 	// DRIVER Controller Values-----------------------------]
@@ -94,6 +97,7 @@ public class Constants {
 		public static final int DRIVE_LEFT1_ID = 1;  
 		public static final int DRIVE_LEFT2_ID = 2;  
 		public static final int DRIVE_LEFT3_ID = 3;  
+		
 		public static final int DRIVE_RIGHT1_ID = 4;  
 		public static final int DRIVE_RIGHT2_ID = 5; 
 		public static final int DRIVE_RIGHT3_ID = 6;  
@@ -113,23 +117,25 @@ public class Constants {
 		public static int ELEVATOR_ROCKET_LEVEL_ONE_CARGO_VALUE = 10;
 		public static int ELEVATOR_ROCKET_LEVEL_TWO_CARGO_VALUE = 1485;
 		public static int ELEVATOR_ROCKET_LEVEL_THREE_CARGO_VALUE = 3645;
+		
 		public static int ELEVATOR_ROCKET_LEVEL_ONE_HATCH_VALUE = 425;
 		public static int ELEVATOR_ROCKET_LEVEL_TWO_HATCH_VALUE = 2745;
 		public static int ELEVATOR_ROCKET_LEVEL_THREE_HATCH_VALUE = 4865;
+		
 		public static int ELEVATOR_INTAKE_METHOD_VALUE = 300;
-
 		public static int ELEVATOR_RESET_ELEVATOR_VALUE = 10;
+		
 		public static int ELEVATOR_K_TIMEOUT_MS = 10;
 
-		public static double ELEVATOR_KF = 1.705;
-		public static double ELEVATOR_KP = 6;//18.0;
-		public static double ELEVATOR_KI = 0;//0.070;
-		public static double ELEVATOR_KD = 0;//51.0;
-		public static int ELEVATOR_KV = 600;//650;
-		public static int ELEVATOR_KA = 500;//1200;
 		public static int ELEVATOR_PID_SLOT_NUMBER = 0;
-
-		public static int ELEVATOR_REVERSE_MULTIPLIER = -1;//1;
+		public static double ELEVATOR_KF_VALUE = 1.705;
+		public static double ELEVATOR_KP_VALUE = 6; //18.0;
+		public static double ELEVATOR_KI_VALUE = 0; //0.070;
+		public static double ELEVATOR_KD_VALUE = 0; //51.0;
+		public static int ELEVATOR_KV_VALUE = 600; //650;
+		public static int ELEVATOR_KA_VALUE = 500; //1200;
+		
+		public static int ELEVATOR_REVERSE_MULTIPLIER = -1; //1;
 		public static int ELEVATOR_MANUAL_MOTION_MAGIC_MULTIPLIER = -50;
 	// Elevator Constants-----------------------------------]
 
@@ -142,8 +148,8 @@ public class Constants {
 		public static final int ENDGAME_ENDGAMELIFTFOLLOW1_ID = 6;
 		public static final int ENDGAME_ENDGAMELIFTFOLLOW2_ID = 7;
 
-		public static final double ENDGAME_WHEEL_SPEED = 0.1;
 		public static final double ENDGAME_LIFT_SPEED = 1.0;
+		public static final double ENDGAME_WHEEL_SPEED = 0.1;
 		public static final double ENDGAME_STOP_SPEED = 0.0;
 
 		public static final double ENDGAME_MAX_TICKS = 9000;
@@ -161,9 +167,9 @@ public class Constants {
 	// Intake Arm Constants--------------------------------[
 		public static final int ARM_SOLENOID_PORT = 3;
 
+		public static final boolean ARM_STATE_DOWN = true;
 		public static final boolean ARM_START_POSITION = true;
 		public static final boolean ARM_STATE_UP = false;
-		public static final boolean ARM_STATE_DOWN = true;
 	// Intake Arm Constants--------------------------------]
 
 	// Intake Floor Hatch Constants--------------------------------[
@@ -185,18 +191,20 @@ public class Constants {
 	// Cargo Intake Constants------------------------------[
 		public static final int CARGO_SOLENOID_PORT = 4;
 		public static final int CARGO_STOP_INTAKE_SOLENOID_PORT = 5;
-		public static final int CARGO_VICTORSPX_PORT = 4;
-		public static final boolean CARGO_START_POSITION = false;
-		public static final double CARGO_INTAKE_SPEED = -0.8;
-		public static final double CARGO_OUTTAKE_SPEED = 0.5;
-		public static final double CARGO_UP_OUTTAKE_SPEED = 1;
-		public static final double CARGO_STOP_SPEED = 0.0;
-		public static final boolean CARGO_HAND_DOWN = true;
-		public static final boolean CARGO_HAND_UP = false;
-		public static final boolean CARGO_BRAKE = true;
-		public static final boolean CARGO_RELEASE = false;
+		public static final int CARGO_VICTORSPX_ID = 4;
+		
 		public static final int CARGO_BRAKE_DELAY = 3;
+		public static final double CARGO_UP_OUTTAKE_SPEED = 1;
+		public static final double CARGO_OUTTAKE_SPEED = 0.5;
+		public static final double CARGO_STOP_SPEED = 0.0;
 		public static final int CARGO_BRAKE_TIMER_RESET = 0;
+		public static final double CARGO_INTAKE_SPEED = -0.8;
+		
+		public static final boolean CARGO_HAND_DOWN = true;
+		public static final boolean CARGO_BRAKE = true;
+		public static final boolean CARGO_START_POSITION = false;
+		public static final boolean CARGO_HAND_UP = false;
+		public static final boolean CARGO_RELEASE = false;
 	// Cargo Intake Constants------------------------------]
 
 	// Camera-----------------------------------------------[
@@ -206,6 +214,7 @@ public class Constants {
 		public static final int CAMERA1_WIDTH = 160;
 		public static final int CAMERA1_HEIGHT = 120;
 		public static final int CAMERA1_FPS = 30;
+		
 		public static final int CAMERA2_WIDTH = 160;
 		public static final int CAMERA2_HEIGHT = 120;
 		public static final int CAMERA2_FPS = 30;
