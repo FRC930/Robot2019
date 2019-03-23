47ba07de5c53ae9b5591494a4b0f0bb6a4e44340
MasterMemeMinister <soupcateatternom@gmail.com>
2019-03-23 06:48:21 -0500

    Made changes for Day 2 Iowa

--- RobotCode2019/src/main/java/frc/robot/Constants.java
+++ RobotCode2019/src/main/java/frc/robot/Constants.java
@@ -236,7 +236,7 @@ public class Constants {
 		
 		public static final int CARGO_BRAKE_DELAY = 4;
 		public static final double CARGO_UP_OUTTAKE_SPEED = 1;
-		public static final double CARGO_OUTTAKE_SPEED = 0.8;//0.5;
+		public static final double CARGO_OUTTAKE_SPEED = 0.5;//0.5;
 		public static final double CARGO_STOP_SPEED = 0.0;
 		public static final int CARGO_BRAKE_TIMER_RESET = 0;
 		public static final double CARGO_INTAKE_SPEED = -0.8;
