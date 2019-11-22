b5b746c0b17603a455cd50cb7cfd72c6cf5a8473
awtpi314 <52684858+awtpi314@users.noreply.github.com>
2019-11-21 18:20:46 -0600

    blah

--- RobotCode2019/src/test/java/frc/robot/DriveTest.java
+++ RobotCode2019/src/test/java/frc/robot/DriveTest.java
@@ -1,12 +1,15 @@
 package frc.robot;
 
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
-
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.mockito.Answers;
+import org.mockito.ArgumentMatcher;
 
 /**
  * DriveTest
@@ -14,6 +17,20 @@ import org.junit.runner.RunWith;
 public class DriveTest {
     @Test
     public void testDeadband() throws Exception {
-        CANSparkMax left1 = mock(CANSparkMax.class);
+        // CANSparkMax left1 = mock(CANSparkMax.class, withSettings()
+        //         .useConstructor(Constants.DRIVE_LEFT1_ID, MotorType.kBrushless).defaultAnswer(Answers.RETURNS_MOCKS));
+        // CANSparkMax right1 = mock(CANSparkMax.class, withSettings()
+        //         .useConstructor(Constants.DRIVE_RIGHT1_ID, MotorType.kBrushless).defaultAnswer(Answers.RETURNS_MOCKS));
+        // Drive drive = mock(Drive.class, withSettings().useConstructor(left1, left1, left1, right1, right1, right1));
+        // drive.run(0.000123, 0);
     }
+}
+
+class NonZeroDouble implements ArgumentMatcher<Double> {
+
+    @Override
+    public boolean matches(Double argument) {
+        return argument == 0;
+    }
+
 }
